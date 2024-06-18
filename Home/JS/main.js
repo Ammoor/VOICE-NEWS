74363cc9de42b84e7052a82227ef9d14da1655db
Omar-Ayman-Gaber <149304486+Omar-Ayman-Gaber@users.noreply.github.com>
2024-06-18 12:06:03 +0300

    Update main.js

--- Home/JS/main.js
+++ Home/JS/main.js
@@ -4,9 +4,32 @@ window.onload = function () {
 };
 function toggleAudio() {
   var audioElement = document.getElementById("audio-theme-track");
+  var button_content = document.querySelector(
+    ".audio-theme-button-container .audio-theme-button"
+  );
   if (audioElement.paused == true) {
     audioElement.play();
+    document.querySelector(
+      ".audio-theme-button-container .audio-theme-button"
+    ).style.backgroundColor = "crimson";
+    document.querySelector(
+      ".audio-theme-button-container .audio-theme-button"
+    ).style.color = "white";
+    document.querySelector(
+      ".audio-theme-button-container .audio-theme-button"
+    ).style.transition = "1.5s";
+    button_content.innerHTML = "Unplay sound track";
   } else {
     audioElement.pause();
+    document.querySelector(
+      ".audio-theme-button-container .audio-theme-button"
+    ).style.backgroundColor = "yellow";
+    document.querySelector(
+      ".audio-theme-button-container .audio-theme-button"
+    ).style.color = "black";
+    document.querySelector(
+      ".audio-theme-button-container .audio-theme-button"
+    ).style.transition = "1.5s";
+    button_content.innerHTML = "Play sound track";
   }
 }
